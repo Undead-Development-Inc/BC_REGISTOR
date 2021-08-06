154b6b579a45b94d1a5fe5b78622ca6bf5d8e894
mjolyrice <jolyricem@gmail.com>
2021-08-05 20:40:32 -0600

    Fixed Typo

--- out/artifacts/BC_REGISTOR_jar/run.sh
+++ out/artifacts/BC_REGISTOR_jar/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git pull;
-nohup -java -jar /usr/BC_REGISTOR/out/artifacts/BC_REGISTOR_jar/BC_REGISTOR.jar
+nohup java -jar /usr/BC_REGISTOR/out/artifacts/BC_REGISTOR_jar/BC_REGISTOR.jar
